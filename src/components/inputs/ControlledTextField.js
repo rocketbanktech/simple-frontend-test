d15ac036d38c88d27b9900a04e0037ac281be20c
Eduardo Souza <dado.eletrons@gmail.com>
2021-09-20 14:51:17 -0300

    :recycle: refactoring: The component <ControlledTextField.js/>

--- src/components/inputs/ControlledTextField.js
+++ src/components/inputs/ControlledTextField.js
@@ -3,38 +3,38 @@ import { TextField as MTextField } from "@material-ui/core";
 
 import { Controller } from "react-hook-form";
 
-const ControlledTextField = ({
-  formProps,
-  name,
-  ...otherProps
-}) => {
-  const { control, errors, rules, initialValues } = formProps;
-  const isError = errors[name] !== undefined;
+const ControlledTextField = React.forwardRef(
+  ({ formProps, name, ...otherProps }, ref) => {
+    const { control, errors, rules, initialValues } = formProps;
+    const isError = errors[name] !== undefined;
 
-  return (
-    <Controller
-      name={name}
-      control={control}
-      rules={rules[name]}
-      defaultValue={initialValues[name]}
-      render={({ onChange, onBlur, value }) => (
-        <MTextField
-          {...otherProps}
-          value={value}
-          error={isError}
-          helperText={errors[name]?.message}
-          onChange={(v) => {
-            onChange(v);
-            !!otherProps.onChange && otherProps.onChange(v);
-          }}
-          onBlur={() => {
-            onBlur();
-            !!otherProps.onBlur && otherProps.onBlur(value);
-          }}
-        />
-      )}
-    />
-  );
-};
+    return (
+      <Controller
+        name={name}
+        control={control}
+        rules={rules[name]}
+        defaultValue={initialValues[name]}
+        render={({ onChange, onBlur, value }) => (
+          <MTextField
+            {...otherProps}
+            value={value}
+            error={isError}
+            variant="outlined"
+            inputRef={ref}
+            helperText={errors[name]?.message}
+            onChange={(v) => {
+              onChange(v);
+              !!otherProps.onChange && otherProps.onChange(v);
+            }}
+            onBlur={() => {
+              onBlur();
+              !!otherProps.onBlur && otherProps.onBlur(value);
+            }}
+          />
+        )}
+      />
+    );
+  }
+);
 
 export default ControlledTextField;
